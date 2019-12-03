9fb69f962d695ce25e87698878ed9a26e5b78b5a
Daniel Perez Jensen <danielperezjensen@gmail.com>
2019-12-03 14:33:25 +0100

    Added first swing at rank analysis using a pairwise test

---
+++ analyses/rank_analysis.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+import random
+
+
+def pairwise_test(x, y, n):
+    """Draw a sample from x and y, n times and compare their values.
+    Returns the fraction of times that the value drawn from x is larger
+    than the value drawn from y."""
+    count = 0
+    for _ in range(n):
+        x_random = random.choice(x)
+        y_random = random.choice(y)
+        if x_random > y_random:
+            count += 1
+    return count / n
+
+
+def get_colonist(cc3):
+    """Returns the 3 letter country code of the colonizer of cc3"""
+    english_colonies = ['EGY', 'KEN', 'NGA', 'ZAF', 'ZMB', 'ZWE', 'MUS']
+    french_colonies = ['DZA', 'CIV', 'CAF', 'MAR', 'TUN']
+    portuguese_colonies = ['AGO']
+
+    if cc3 in portuguese_colonies:
+        return 'PRT'
+    elif cc3 in english_colonies:
+        return 'GBR'
+    elif cc3 in french_colonies:
+        return 'FRA'
+
+
+crises_df = pd.read_csv('../data/african_crises.csv')
+
+# Replace values in banking_crisis with boolean values
+crises_df = crises_df.replace({'banking_crisis': {'crisis': 1, 'no_crisis': 0}})
+
+# Columns that signify what rows/years had crises
+crises_cols = ['systemic_crisis', 'domestic_debt_in_default',
+               'currency_crises', 'inflation_crises',
+               'banking_crisis']
+
+
+crises_df_after_1957 = crises_df.loc[crises_df['year'] > 1957]
+
+# Gather all boolean crises from after 1957
+bool_crises_df = crises_df_after_1957[['systemic_crisis',
+                                       'domestic_debt_in_default',
+                                       'currency_crises', 'inflation_crises',
+                                       'banking_crisis', 'cc3']]
+
+# Repeat n pairwise tests m times
+n, m = 10000, 1000
+
+# If the British ranks aren't statistically higher ranked than the French
+# we would expect the mean of our test to fall within the 95%-CI of
+# a binomial distribution with p=0.5.
+binomial_samples = [np.random.binomial(n, 0.5) / n for _ in range(m)]
+
+FRA_samples, GBR_samples = [], []
+ccs = crises_df['cc3'].unique()
+# For each country save the sum (amount of crisis years)
+for crisis in crises_cols:
+    for cc in ccs:
+        if get_colonist(cc) == 'FRA':
+            FRA_samples.append(bool_crises_df[crisis]
+                               .loc[bool_crises_df['cc3'] == cc].sum())
+        if get_colonist(cc) == 'GBR':
+            GBR_samples.append(bool_crises_df[crisis]
+                               .loc[bool_crises_df['cc3'] == cc].sum())
+
+    # Plotting stuff
+    plt.title(crisis)
+    plt.xlabel('Percentage GBR > FRA')
+    plt.ylabel('Count')
+    plt.hist(binomial_samples)
+
+    # Calculate the mean of our pairwise_test
+    pairwise_mean = np.mean([pairwise_test(GBR_samples, FRA_samples, n)
+                             for _ in range(m)])
+
+    # Plot confidence interval and our calculated mean
+    plt.axvline(pairwise_mean, c='k', linestyle='--', label='pairwise mean')
+    plt.axvline(np.percentile(binomial_samples, 1.25), color='g',
+                label='CI', linestyle=':')
+    plt.axvline(np.percentile(binomial_samples, 97.5), color='g',
+                linestyle=':')
+
+    plt.legend()
+    plt.show()
