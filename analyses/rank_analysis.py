fa1c8a1485ed6c4c7ce194ffbfa1a8cbb96726ad
Daniel Perez Jensen <danielperezjensen@gmail.com>
2019-12-04 22:56:53 +0100

    Taking the mean of the resamples now works properly

--- analyses/rank_analysis.py
+++ analyses/rank_analysis.py
@@ -39,18 +39,16 @@ crises_df = crises_df.replace({'banking_crisis':
                                {'crisis': 1, 'no_crisis': 0}})
 
 # Columns that signify what rows/years had crises
-crises_cols = ['systemic_crisis', 'domestic_debt_in_default',
-               'currency_crises', 'inflation_crises',
+crises_cols = ['systemic_crisis', 'currency_crises', 'inflation_crises',
                'banking_crisis']
 
 
 crises_df_after_1957 = crises_df.loc[crises_df['year'] > 1957]
 
 # Gather all boolean crises from after 1957
-bool_crises_df = crises_df_after_1957[['systemic_crisis',
-                                       'domestic_debt_in_default',
-                                       'currency_crises', 'inflation_crises',
-                                       'banking_crisis', 'cc3']]
+bool_crises_df = crises_df_after_1957[['systemic_crisis', 'currency_crises',
+                                       'inflation_crises', 'banking_crisis',
+                                       'cc3']]
 
 # Repeat n pairwise tests m times
 n, m = 10000, 1000
@@ -73,32 +71,33 @@ for crisis in crises_cols:
             GBR_sample.append(bool_crises_df[crisis]
                               .loc[bool_crises_df['cc3'] == cc].sum())
 
-    # Calculate the mean of our pairwise_test
+    # Calculate the mean of m pairwise tests
     true_pairwise_mean = np.mean([pairwise_test(FRA_sample, GBR_sample, n)
                                   for _ in range(m)])
 
     # Resample our data using a multinomial distribution
-    gbr_p_values = np.array(GBR_sample) / len(bool_crises_df)
-    fra_p_values = np.array(FRA_sample) / len(bool_crises_df)
+    gbr_p_values = np.array(GBR_sample) / sum(GBR_sample)
+    fra_p_values = np.array(FRA_sample) / sum(FRA_sample)
 
     # TODO: Not sure if it should be length of sum
-    gbr_resamples = np.random.multinomial(len(bool_crises_df), gbr_p_values,
+    gbr_resamples = np.random.multinomial(sum(GBR_sample), gbr_p_values,
                                           size=50)
-    fra_resamples = np.random.multinomial(len(bool_crises_df), fra_p_values,
+    fra_resamples = np.random.multinomial(sum(FRA_sample), fra_p_values,
                                           size=50)
 
     # Zip our samples together so we can use it with our pairwise test function
     zipped_resamples = zip(fra_resamples, gbr_resamples)
 
     # Run our pairwise test on each of the 50 new samples
-    resampled_pairwise_tests = [pairwise_test(f, g, n) for
-                                f, g in zipped_resamples]
+    resampled_pairwise_tests = [np.mean([pairwise_test(f, g, n)
+                                         for _ in range(m)])
+                                for f, g in zipped_resamples]
 
     # Plotting stuff
     plt.title(crisis)
     plt.xlabel('Percentage FRA > GBR')
     plt.ylabel('Count')
-    plt.hist(binomial_samples)
+    plt.hist(binomial_samples, bins=int(np.sqrt(m)))
 
     # Plot confidence interval and our calculated mean
     plt.axvline(true_pairwise_mean, c='k', linestyle='--',
@@ -108,8 +107,9 @@ for crisis in crises_cols:
     plt.axvline(np.percentile(binomial_samples, 97.5), color='g',
                 linestyle=':')
 
-    # Plot each of our resamples in magenta and low opaqueness
+    # Plot each of our resampled means in magenta and low opaqueness
     for resampled_test in resampled_pairwise_tests:
         plt.axvline(resampled_test, c='m', linestyle='-', alpha=0.25)
+
     plt.legend()
     plt.show()
